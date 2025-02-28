ac56ce5ad872932f15fd17f9323501d711759b9e
Eric Delattre <edelattre@gmail.com>
2025-02-28 22:29:47 +0700

    Add retirement simulator with complete functionality including investment calculations, retirement planning, and interactive UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SimulateurRetraite from './components/RetirementSimulator';
+
+function App() {
+  return (
+    <div className="App">
+      <SimulateurRetraite />
+    </div>
+  );
+}
+
+export default App;
